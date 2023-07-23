a0909c135f36a7919069b35f888220fde143ba2d
haikaljs <haikaljavascript@gmail.com>
2023-07-23 10:06:36 +0800

    Styling container, wrapper container

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+"use client";
+
+import Container from "../Container";
 
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
-        
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            
+          </div>
+        </Container>
       </div>
     </div>
   );
