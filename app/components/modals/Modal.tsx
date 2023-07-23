a6f179f6881dd68da59bd4dfa165422ae53ea741
haikaljs <haikaljavascript@gmail.com>
2023-07-24 00:19:01 +0800

    Passing props from modal to button

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -92,7 +92,11 @@ const Modal: React.FC<ModalProps> = ({
               {/* FOOTER */}
               <div className="flex flex-col gap-2 p-6">
                 <div className="flex flex-row items-center gap-4 w-full">
-                  <Button icon={IoMdClose} label="My button" />
+                  <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubmit}
+                  />
                 </div>
               </div>
             </div>
